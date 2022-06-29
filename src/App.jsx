b18799b865307fbf9b2d60c54ce49d7c21485538
Felipe Neves Ricardo <contato@felipenr.com>
2022-06-29 05:44:50 -0300

    Post component intial implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+import { Post } from "./Post";
+
 export function App() {
 
   return (
-    <h1>Hello World!</h1>
+    <div>
+      <Post
+        author="Felipe Neves Ricardo"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum distinctio totam, temporibus debitis ratione quod cum. Tempore est, illum repudiandae velit pariatur vitae aperiam, atque porro sunt dolorum deleniti quisquam."
+      />
+      <Post
+        author="Filipe Roberto Beck"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum distinctio totam, temporibus debitis ratione quod cum. Tempore est, illum repudiandae velit pariatur vitae aperiam, atque porro sunt dolorum deleniti quisquam."
+      />
+    </div>
   )
 }
