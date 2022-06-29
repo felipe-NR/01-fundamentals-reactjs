3c92e79eeae0c0ca3c26f4d6f9d93041e29c8c79
Felipe Neves Ricardo <contato@felipenr.com>
2022-06-29 16:16:48 -0300

    Header component implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
 import React from "react"
+import styles from "./App.module.css"
 import { Header } from "./components/Header"
-import { Post } from "./Post"
-import "./styles.css"
+import { Post } from "./components/Post"
+import "./global.css"
 
 export function App() {
 
   return (
     <div>
       <Header/>
-      <Post
-        author="Felipe Neves Ricardo"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum distinctio totam, temporibus debitis ratione quod cum. Tempore est, illum repudiandae velit pariatur vitae aperiam, atque porro sunt dolorum deleniti quisquam."
-      />
-      <Post
-        author="Filipe Roberto Beck"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum distinctio totam, temporibus debitis ratione quod cum. Tempore est, illum repudiandae velit pariatur vitae aperiam, atque porro sunt dolorum deleniti quisquam."
-      />
+      <div className={styles.wrapper} >
+        <aside>
+          Sidebar
+        </aside>
+        <main>
+          <Post
+            author="Felipe Neves Ricardo"
+            content="Lorem ipsum dolor sit amet consectetur     adipisicing elit. Dolorum distinctio totam, temporibus debitis ratione quod cum. Tempore est, illum repudiandae velit pariatur vitae aperiam, atque porro sunt dolorum deleniti quisquam."
+          />
+          <Post
+            author="Filipe Roberto Beck"
+            content="Lorem ipsum dolor sit amet consectetur     adipisicing elit. Dolorum distinctio totam, temporibus debitis ratione quod cum. Tempore est, illum repudiandae velit pariatur vitae aperiam, atque porro sunt dolorum deleniti quisquam."
+          />
+        </main>
+      </div>      
     </div>
   )
 }
