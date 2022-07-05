bc2a3a6c91e03782a65a9f4ce3c74a0141ea4445
Felipe Neves Ricardo <contato+github@felipenr.com>
2022-07-05 18:12:30 -0300

    Sidebar component implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,19 @@
 import React from "react"
 import styles from "./App.module.css"
 import { Header } from "./components/Header"
-import { Post } from "./components/Post"
+import { Sidebar } from "./components/Sidebar"
 import "./global.css"
 
 export function App() {
 
   return (
     <div>
-      <Header/>
+      <Header />
       <div className={styles.wrapper} >
-        <aside>
-          Sidebar
-        </aside>
+        <Sidebar />
         <main>
-          <Post
-            author="Felipe Neves Ricardo"
-            content="Lorem ipsum dolor sit amet consectetur     adipisicing elit. Dolorum distinctio totam, temporibus debitis ratione quod cum. Tempore est, illum repudiandae velit pariatur vitae aperiam, atque porro sunt dolorum deleniti quisquam."
-          />
-          <Post
-            author="Filipe Roberto Beck"
-            content="Lorem ipsum dolor sit amet consectetur     adipisicing elit. Dolorum distinctio totam, temporibus debitis ratione quod cum. Tempore est, illum repudiandae velit pariatur vitae aperiam, atque porro sunt dolorum deleniti quisquam."
-          />
+          <h1>Post</h1>
+          <h1>Post</h1>
         </main>
       </div>      
     </div>
